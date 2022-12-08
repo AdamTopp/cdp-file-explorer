63a9290618aff482f4672c34ebb19588c2a2421c
AdamTopp <90351456+AdamTopp@users.noreply.github.com>
2022-12-09 00:17:14 +0100

    Fully working configuration (#2)
    * Eslint setup
    
    * Fix prettier configuration and add tree shaking
    
    * Fix code
    
    * Starting the app concurrently and add Styled Components
    
    * Fully working configuration setup
    
    Co-authored-by: AdamTopmay <adatop1@st.amu.edu.pl>

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Dd = styled.div`
+  color: red;
+`;
+
+function App() {
+  return <Dd>Apppppp</Dd>;
+}
+
+export default App;
